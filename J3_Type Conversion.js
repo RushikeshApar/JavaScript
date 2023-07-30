441e18b6266a67c3e9ce8a586d7b27c303d4d8f7
Rushikesh Apar <rushikeshapar.apar@gmail>
2023-07-30 14:04:37 +0530

    type conversion in js

---
+++ J3_Type Conversion.js
@@ -0,0 +1,15 @@
+console.log("Type Conversion in Js");
+
+let age = 25;
+console.log(typeof age);
+
+
+console.log("type of conversion");
+// =========== type of conversion =============
+// ------- Number(), String(), Boolean()
+
+let a = 15;
+console.log(typeof a); //old
+// converting type number to string
+let b = String(a);
+console.log(typeof b); //new
