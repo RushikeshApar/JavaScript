8155818922c056652bca7091f83c36a4d7649af1
Rushikesh Apar <rushikeshapar.apar@gmail>
2023-08-19 20:55:55 +0530

    prototype

--- Js8_Object_Constructor_Function.js
+++ Js8_Object_Constructor_Function.js
@@ -7,30 +7,34 @@ let student1 = {
   class: 12,
 };
 
-console.log("noraml obj :", student1);
+console.log(" this is a noraml obj :", student1);
+//this is a noraml obj
 
 // like this if we want to make multiple obj then we have to write multiple obj that's why  Object Constructor Function came .
 
 console.log("Object Constructor Function & prototype");
 function student(first, last, age, clas) {
-    this.firstName = first,
-    this.lastName = last,
-    this.age = age,
-    this.class = clas
+  (this.firstName = first),
+    (this.lastName = last),
+    (this.age = age),
+    (this.class = clas);
+  this.name = function () {
+    return this.firstName + " " + this.lastName;
+  };
 }
-
 student1 = new student("Tanvika", "Raut", 25, 4);
 student2 = new student("jeet", "Dank", 25, 4);
 student3 = new student("pratik", "jadhav", 18, 1);
-student4 = new student("jayash", "lahare", 28, 15);
+student4 = new student("Anna", "Apar", 28, 15);
 
 console.log(student1, student2, student3, student4);
+
+console.log("Adding function in Constructor obj :", student1.name());
 
 // prtotype is used for when we want to add new properties in all object at a time
 student.prototype.nationality = "INDIAN";
 
-
-
-
-
-
+student.prototype.Fullname = function () {
+  return this.firstName + " " + this.lastName;
+};
+console.log("Adding function by Ptotoype  :", student4.Fullname());
