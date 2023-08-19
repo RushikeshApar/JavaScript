b2f4221fbe14a14e477d2f2eef245a2563fb2276
Rushikesh Apar <rushikeshapar.apar@gmail>
2023-08-19 19:34:40 +0530

    Object Constructor Function & prototype

--- Js8_Object_Constructor_Function.js
+++ Js8_Object_Constructor_Function.js
@@ -1,35 +1,36 @@
-console.log("Object Constructor Function");
+console.log("Objectn");
 
-let student1  ={
-    firstName:"Rushikesh",
-    lastName:"Apar",
-    age:26,
-    class: 12
-}
+let student1 = {
+  firstName: "Rushikesh",
+  lastName: "Apar",
+  age: 26,
+  class: 12,
+};
 
-console.log(student1);
+console.log("noraml obj :", student1);
 
 // like this if we want to make multiple obj then we have to write multiple obj that's why  Object Constructor Function came .
 
-function student(first,last,age,clas){
-    this.firstName = first,
-    this.lastName = last,
-    this.age = age,
-    this.class = clas
+console.log("Object Constructor Function & prototype");
+function student(first, last, age, clas) {
+  (this.firstName = first),
+    (this.lastName = last),
+    (this.age = age),
+    (this.class = clas);
 }
 
+student1 = new student("Tanvika", "Raut", 25, 4);
+student2 = new student("jeet", "Dank", 25, 4);
+student3 = new student("pratik", "jadhav", 18, 1);
+student4 = new student("jayash", "lahare", 28, 15);
 
-student2 = new student("Rahul","Kumar",20,8)
-console.log(student2);
+console.log(student1, student2, student3, student4);
 
-student2 = new student("jeet","Dank",25,4)
-student3 = new student("pratik","jadhav",18,1)
-student4 = new student("jayash","lahare",28,15)
+// prtotype is used for when we want to add new properties in all object at a time
+student.prototype.nationality = "INDIAN";
 
-console.log(student1,student2,student3,student4);
 
-student1.name = function(){
-    return this.firstName +" "+ this.lastName
-} 
 
-console.log(student1.name());
+
+
+
